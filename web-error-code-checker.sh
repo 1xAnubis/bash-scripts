5814470cc0124f8d7ceb117abcb6594e4b44cd1b
Mhmmd Ashraf <medo.elkasaby55@gmail.com>
2023-04-24 13:20:28 +0200

    Add validate for input

--- web-error-code-checker.sh
+++ web-error-code-checker.sh
@@ -1,80 +1,97 @@
 #!/bin/bash
 
-error_codes=(
-    [100]='Continue'
-    [101]='Switching Protocols'
-    [102]='Processing'
-    [103]='Early Hints'
-    [200]='OK'
-    [201]='Created'
-    [202]='Accepted'
-    [203]='Non-Authoritative Information'
-    [204]='No Content'
-    [205]='Reset Content'
-    [206]='Partial Content'
-    [207]='Multi-Status'
-    [208]='Already Reported'
-    [226]='IM Used'
-    [300]='Multiple Choices'
-    [301]='Moved Permanently'
-    [302]='Found'
-    [303]='See Other'
-    [304]='Not Modified'
-    [305]='Use Proxy'
-    [307]='Temporary Redirect'
-    [308]='Permanent Redirect'
-    [400]='Bad Request'
-    [401]='Unauthorized'
-    [402]='Payment Required'
-    [403]='Forbidden'
-    [404]='Not Found'
-    [405]='Method Not Allowed'
-    [406]='Not Acceptable'
-    [407]='Proxy Authentication Required'
-    [408]='Request Timeout'
-    [409]='Conflict'
-    [410]='Gone'
-    [411]='Length Required'
-    [412]='Precondition Failed'
-    [413]='Payload Too Large'
-    [414]='URI Too Long'
-    [415]='Unsupported Media Type'
-    [416]='Range Not Satisfiable'
-    [417]='Expectation Failed'
-    [418]="I'm a teapot"
-    [421]='Misdirected Request'
-    [422]='Unprocessable Entity'
-    [423]='Locked'
-    [424]='Failed Dependency'
-    [425]='Too Early'
-    [426]='Upgrade Required'
-    [428]='Precondition Required'
-    [429]='Too Many Requests'
-    [431]='Request Header Fields Too Large'
-    [451]='Unavailable For Legal Reasons'
-    [500]='Internal Server Error'
-    [501]='Not Implemented'
-    [502]='Bad Gateway'
-    [503]='Service Unavailable'
-    [504]='Gateway Timeout'
-    [505]='HTTP Version Not Supported'
-    [506]='Variant Also Negotiates'
-    [507]='Insufficient Storage'
-    [508]='Loop Detected'
-    [510]='Not Extended'
-    [511]='Network Authentication Required'
-)
+validate_input() {
+    if [[ ! $1 =~ ^[0-9]+$ ]]; then
+        echo "Error: Invalid input. Please enter an integer." >&2
+        exit 1
+    elif [[ $1 -lt 100 ]] || [[ $1 -gt 599 ]]; then
+        echo "Error: Invalid error code. Please enter a code between 100 and 599." >&2
+        exit 1
+    fi
+}
 
-read -p 'Enter the web error code: ' error_code
+get_error_message() {
+    error_code=$1
+    case $error_code in 
+    100) echo "Continue";;
+    101) echo "Switching Protocols";;
+    102) echo "Processing";;
+    103) echo "Early Hints";;
+    200) echo "OK";;
+    201) echo "Created";;
+    202) echo "Accepted";;
+    203) echo "Non-Authoritative Information";;
+    204) echo "No Content";;
+    205) echo "Reset Content";;
+    206) echo "Partial Content";;
+    207) echo "Multi-Status";;
+    208) echo "Already Reported";;
+    226) echo "IM Used";;
+    300) echo "Multiple Choices";;
+    301) echo "Moved Permanently";;
+    302) echo "Found";;
+    303) echo "See Other";;
+    304) echo "Not Modified";;
+    305) echo "Use Proxy";;
+    307) echo "Temporary Redirect";;
+    308) echo "Permanent Redirect";;
+    400) echo "Bad Request";;
+    401) echo "Unauthorized";;
+    402) echo "Payment Required";;
+    403) echo "Forbidden";;
+    404) echo "Not Found";;
+    405) echo "Method Not Allowed";;
+    406) echo "Not Acceptable";;
+    407) echo "Proxy Authentication Required";;
+    408) echo "Request Timeout";;
+    409) echo "Conflict";;
+    410) echo "Gone";;
+    411) echo "Length Required";;
+    412) echo "Precondition Failed";;
+    413) echo "Payload Too Large";;
+    414) echo "URI Too Long";;
+    415) echo "Unsupported Media Type";;
+    416) echo "Range Not Satisfiable";;
+    417) echo "Expectation Failed";;
+    418) echo "I'm a teapot";;
+    421) echo "Misdirected Request";;
+    422) echo "Unprocessable Entity";;
+    423) echo "Locked";;
+    424) echo "Failed Dependency";;
+    425) echo "Too Early";;
+    426) echo "Upgrade Required";;
+    428) echo "Precondition Required";;
+    429) echo "Too Many Requests";;
+    431) echo "Request Header Fields Too Large";;
+    451) echo "Unavailable For Legal Reasons";;
+    500) echo "Internal Server Error";;
+    501) echo "Not Implemented";;
+    502) echo "Bad Gateway";;
+    503) echo "Service Unavailable";;
+    504) echo "Gateway Timeout";;
+    505) echo "HTTP Version Not Supported";;
+    506) echo "Variant Also Negotiates";;
+    507) echo "Insufficient Storage";;
+    508) echo "Loop Detected";;
+    510) echo "Not Extended";;
+    511) echo "Network Authentication Required";;
+    *) echo "Unkown error: $error_code";;
+    esac
+}
 
-if [[ -z ${error_codes[$error_code]} ]]; then
-    echo "Unknown error: $error_code"
-else
-    echo "The error message for error code $error_code is: ${error_codes[$error_code]}"
-fi
 
-echo -e "\n========================================"
-echo -e "          WEB ERROR MESSAGE"
-echo -e "             by:Anubis-369"
-echo -e "========================================\n"
-echo "The error message for error code $error_code is: ${error_codes[$error_code]}"
+program_name="WEB ERROR MESSAGE"
+author_name="by: Anubis-369"
+error_message_prefix="the error message for error code"\
+
+echo -e "\n========================================="
+echo -e "\t\t$program_name"
+echo -e "\t\t   $author_name"
+echo -e "=========================================\n"
+read -p "Enter the web error code: " error_code
+validate_input "$error_code"
+error_message=$(get_error_message "$error_code")
+echo -e "$error_message_prefix $error_code is:\n"
+cat << EOF
+$error_message
+EOF
